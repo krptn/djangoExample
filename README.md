f8d97e2a3a36eb2a37b18fe4dbde104f159a838f
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-08-27 17:10:34 +0200

    add django stuff, ready to add krypton stuff

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Example Krypton Integration with Django
 
 Please see our [documentation for details](https://docs.krptn.dev/).
+
+This is an adapted version of [this totorial](https://learndjango.com/tutorials/django-login-and-logout-tutorial).
--- djangoExample/settings.py
+++ djangoExample/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGOUT_REDIRECT_URL = "/" 
+LOGIN_REDIRECT_URL = "/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'djangoExample.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
