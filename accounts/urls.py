6ecbb1342434ff1185d22ee241764d586e1fdff8
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-05 10:50:35 +0200

    fix a bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView, LoginView, RecoveryCodeView, EnableMFA, LogoutView
+from .views import SignUpView, LoginView, EnableMFA, LogoutView
 
 
 urlpatterns = [
     path("signup/", SignUpView, name="signup"),
-    path("reset/", RecoveryCodeView, name="reset"),
     path("login/", LoginView, name="login"),
     path("logout/", LogoutView, name="logout"),
     path("mfa/", EnableMFA, name="mfa")
